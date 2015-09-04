958dbef34d110968ac0474c54fe48bb9459c2f83
mathspanda <mathspanda826@gmail.com>
2015-09-04 23:24:38 +0800

    update mongodb.common

---
+++ cu_kg/mp/db/mongodb/common.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+from pymongo import MongoClient
+
+
+def init_client(host, port):
+    client = MongoClient(host, port)
+    return client
+
+
+def close_client(client):
+    client.close()
+
+
+def get_database(client, name):
+    db = client[name]
+    return db
+
+
+def get_collection(db, name):
+    collection = db[name]
+    return collection
--- cu_kg/mp/db/mongodb/utils.py
+++ cu_kg/mp/db/mongodb/utils.py
@@ -1,50 +1,65 @@
 # -*- coding:utf-8 -*-
 
-from pymongo import MongoClient
-
 from cu_kg.config import MongoDBConfig
+from common import init_client, close_client, get_database, get_collection
+
+QUADS_FIELD = (
+    SUBJECT, PREDICATE, OBJECT, LABEL, TIMESTAMP, URLS
+) = (
+    'subject', 'predicate', 'object', 'label', 'timestamp', 'urls'
+)
 
 
-def init_client():
-    client = MongoClient(MongoDBConfig.MONGODB_HOST,
+def init_db_cayley():
+    client = init_client(MongoDBConfig.MONGODB_HOST,
                          MongoDBConfig.MONGODB_PORT)
-    return client
+    db = get_database(client, MongoDBConfig.DATABASE_NAME)
+    return client, db
 
 
-def close_client(client):
-    client.close()
+def get_quads_collection(db):
+    return get_collection(db, MongoDBConfig.COLLECTION_QUADS)
 
 
-def get_database(client, name):
-    db = client[name]
-    return db
-
-
-def get_collection(db, name):
-    collection = db[name]
-    return collection
+def get_nodes_collection(db):
+    return get_collection(db, MongoDBConfig.COLLECTION_NODES)
 
 
 def get_quads(subject, predicate, object):
-    pass
+    client, db = init_db_cayley()
+    quads = get_quads_collection(db)
+    result = quads.find_one({SUBJECT: subject, PREDICATE: predicate,
+                             OBJECT: object})
+    close_client(client)
+    return result
 
 
 def update_quads(subject, predicate, object):
     pass
 
 
-def delete_quads(subject, predocate, object):
+def delete_quads(subject, predicate, object):
     pass
 
 
 def get_quads_count():
-    pass
+    client, db = init_db_cayley()
+    nodes = get_nodes_collection(db)
+    count = nodes.find().count()
+    close_client(client)
+    return count
 
 
 def get_nodes_count():
+    client, db = init_db_cayley()
+    nodes = get_nodes_collection(db)
+    count = nodes.find().count()
+    close_client(client)
+    return count
+
+
+if __name__ == '__main__':
     pass
-
-
 
 
 
